329bbac728d7fd7a8d3dd86711f44f05b5dfc5d4
Student <huntergerald00@gmail.com>
2022-05-24 14:59:38 +0200

    drugi

--- app/src/main/java/com/example/projek5/MainActivity.java
+++ app/src/main/java/com/example/projek5/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.projek5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonStart, buttonStop;
+    private TextView textStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonStart = findViewById(R.id.buttonStart);
+        buttonStop = findViewById(R.id.buttonStop);
+        textStatus = findViewById(R.id.textViewStatus);
     }
+
+
 }
