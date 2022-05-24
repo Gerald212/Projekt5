c3e3d2919932e183b9542aed03a848489ef844f2
Student <huntergerald00@gmail.com>
2022-05-24 15:16:17 +0200

    trzeci

--- app/src/main/java/com/example/projek5/MainActivity.java
+++ app/src/main/java/com/example/projek5/MainActivity.java
@@ -3,13 +3,23 @@ package com.example.projek5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button buttonStart, buttonStop;
     private TextView textStatus;
+
+
+    LocationRequest locationRequest;
+    LocationCallback locationCallBack;
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +31,12 @@ public class MainActivity extends AppCompatActivity {
         textStatus = findViewById(R.id.textViewStatus);
     }
 
+    public void buttonStartOnClick(View view){
+        textStatus.setText("Śledzenie włączone");
+    }
+
+    public void buttonStopOnClick(View view){
+        textStatus.setText("Śledzenie wyłączone");
+    }
 
 }
